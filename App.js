6f56701e4b2f429f6009c7390ec3c7f007ddab6f
jy00566722 <jy00566723@gmail.com>
2022-09-17 00:15:41 +0800

    test

--- App.js
+++ App.js
@@ -1,117 +1,56 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {useState} from 'react';
+import {SafeAreaView} from 'react-native';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  Provider,
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogContent,
+  DialogActions,
   Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+} from '@react-native-material/core';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [visible, setVisible] = useState(false);
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <>
+      <SafeAreaView>
+        <Button
+          title="Open Alert Dialog"
+          // eslint-disable-next-line react-native/no-inline-styles
+          style={{margin: 16}}
+          onPress={() => setVisible(true)}
+        />
+        <Dialog visible={visible} onDismiss={() => setVisible(false)}>
+          <DialogHeader title="Dialog Header" />
+          <DialogContent>
+            <Text>这里是一段测试文本哟,哈哈哈哈哈,这是真的吗</Text>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              title="Cancel"
+              compact
+              variant="text"
+              onPress={() => setVisible(false)}
+            />
+            <Button
+              title="Ok"
+              compact
+              variant="text"
+              onPress={() => setVisible(false)}
+            />
+          </DialogActions>
+        </Dialog>
+      </SafeAreaView>
+    </>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const AppProvider = () => (
+  <Provider>
+    <App />
+  </Provider>
+);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default AppProvider;
