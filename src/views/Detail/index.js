6afc7a2e5b5ec3fe4c30b6bf81bfa0b5f3a34444
jy00566722 <jy00566723@gmail.com>
2022-10-13 00:00:40 +0800

    a

--- src/views/Detail/index.js
+++ src/views/Detail/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, Button} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
-export default function Detail() {
+export default function Detail({navigation}) {
   return (
     <SafeAreaView>
       <Text>详情</Text>
+      <Button title="跳到搜索" onPress={() => navigation.navigate('Search')} />
     </SafeAreaView>
   );
 }
--- src/views/main/index.js
+++ src/views/main/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {Button} from '@react-native-material/core';
+import {HStack, Banner, Button, Stack, Chip} from '@react-native-material/core';
 // import MeterialCommunityIcons from 'react-native-vector-icons';
-import Icons from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default function Main() {
-  return (
-    <SafeAreaView>
-      <View style={{alignSelf: 'center', marginTop: 40}}>
-        <Text>首页</Text>
-        <Button title="Click Me" />
-        <Icons name={'home'} size={100} color={'green'} />
-      </View>
-    </SafeAreaView>
-  );
+export default function Main({navigation}) {
+  return <SafeAreaView></SafeAreaView>;
 }
