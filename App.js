8ac8a19c3c09ea168ac2d2684e3763221fff5e33
jy00566722 <jy00566723@gmail.com>
2022-10-16 23:55:47 +0800

    a

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 // import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+// import SplashScreen from 'react-native-splash-screen';
 
 // 获取路由
 import Router from './src/router/index';
 
 const App = () => {
+  useEffect(() => {
+    console.log('APP启动了');
+    // SplashScreen.hide();
+    return () => {};
+  }, []);
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
--- src/views/main/index.js
+++ src/views/main/index.js
@@ -1,10 +1,50 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {HStack, Banner, Button, Stack, Chip} from '@react-native-material/core';
-// import MeterialCommunityIcons from 'react-native-vector-icons';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React, {useState} from 'react';
+import {
+  Button,
+  Text,
+  StyleSheet,
+  StatusBar,
+  View,
+  ImageBackground,
+} from 'react-native';
 
-export default function Main({navigation}) {
-  return <SafeAreaView></SafeAreaView>;
-}
+import Constants from 'expo-constants';
+
+// const image = {uri: 'https://zh-hans.reactjs.org/logo-og.png'};
+const App = () => {
+  // const styleTypes = ['default', 'dark-content', 'light-content'];
+  // const [visibleStatusBar, setVisibleStatusBar] = useState(false);
+  // const [styleStatusBar, setStyleStatusBar] = useState(styleTypes[0]);
+
+  return (
+    <View style={styles.container}>
+      {/* <ImageBackground source={image} style={styles.image}> */}
+      <Text style={styles.text}>美女壁纸</Text>
+      {/* </ImageBackground>Ï */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#8A2Bf0',
+    padding: 0,
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    background: '#000000a0',
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
