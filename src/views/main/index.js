5ac672d94510ee64fefc1796fee0aa06357b7f34
jy00566722 <jy00566723@gmail.com>
2022-11-18 22:50:18 +0800

    a

--- src/views/main/index.js
+++ src/views/main/index.js
@@ -1,26 +1,50 @@
 import React, {useState} from 'react';
-import {
-  Button,
-  Text,
-  StyleSheet,
-  StatusBar,
-  View,
-  ImageBackground,
-} from 'react-native';
-
+import {Text, StyleSheet, View, Image, Dimensions} from 'react-native';
+// import {Box} from '@react-native-material/core';
 import Constants from 'expo-constants';
+import Carousel from 'react-native-reanimated-carousel';
 
-// const image = {uri: 'https://zh-hans.reactjs.org/logo-og.png'};
 const App = () => {
-  // const styleTypes = ['default', 'dark-content', 'light-content'];
-  // const [visibleStatusBar, setVisibleStatusBar] = useState(false);
-  // const [styleStatusBar, setStyleStatusBar] = useState(styleTypes[0]);
-
+  const [todayItem, setTodayItem] = useState([
+    'https://f004.backblazeb2.com/file/oeolii/20220925/1664035542938099MudVdm.JPG',
+    'https://f004.backblazeb2.com/file/oeolii/20220925/1664035548445653Wgxbmk.JPG',
+    'https://f004.backblazeb2.com/file/oeolii/20220925/1664035560549879OhFTiu.JPG',
+    'https://f004.backblazeb2.com/file/oeolii/20220925/1664035624756466gaazMM.JPG',
+    'https://f004.backblazeb2.com/file/oeolii/20220925/1664097045872613hFHWMA.jpeg',
+    'https://f004.backblazeb2.com/file/oeolii/20220925/1664097407352108nyGEPh.jpeg',
+    'https://f004.backblazeb2.com/file/oeolii/20220925/1664097649573533ixDyuL.jpeg',
+    'https://f004.backblazeb2.com/file/oeolii/20220925/1664097371591612ZqXxOB.jpeg',
+    'https://f004.backblazeb2.com/file/oeolii/20220925/1664096966066880ZNjqiE.jpeg',
+  ]);
+  const width = Dimensions.get('window').width;
+  const _render = imgSrc => {
+    console.log(imgSrc);
+    return (
+      <View>
+        <Image
+          style={styles.todayItem}
+          source={{
+            uri: imgSrc,
+          }}
+        />
+      </View>
+    );
+  };
   return (
     <View style={styles.container}>
-      {/* <ImageBackground source={image} style={styles.image}> */}
-      <Text style={styles.text}>美女壁纸</Text>
-      {/* </ImageBackground>Ï */}
+      <Text style={styles.text}>今日推荐</Text>
+      <View>
+        <Carousel
+          loop
+          width={width}
+          height={width / 2}
+          autoPlay={true}
+          data={todayItem}
+          scrollAnimationDuration={1000}
+          onSnapToItem={index => console.log('current index:', index)}
+          renderItem={({item, index}) => _render(item)}
+        />
+      </View>
     </View>
   );
 };
@@ -30,20 +54,32 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-start',
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#8A2Bf0',
+    backgroundColor: '#eee',
     padding: 0,
   },
   text: {
     color: 'white',
-    fontSize: 42,
+    fontSize: 16,
     fontWeight: 'bold',
-    textAlign: 'center',
-    background: '#000000a0',
+    textAlign: 'left',
+    paddingLeft: 8,
   },
-  image: {
+  containerWarp: {
     flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  itemImage: {
+    width: '30%',
+    height: 220,
+    flexGrow: 1,
+    backgroundColor: 'green',
+    padding: 5,
+    margin: 1,
+  },
+  todayItem: {
+    width: 300,
+    height: 600,
   },
 });
 
